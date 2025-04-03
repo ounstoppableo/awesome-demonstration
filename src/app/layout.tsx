82530fdda2bff51782be5406c3b30b8b67602b96
unstoppable840 <unstoppable840@gmail.com>
2025-04-03 22:29:08 +0800

    fix: 移动端大小适配问题

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import StoreProvider from './StoreProvider';
 import { Suspense } from 'react';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -35,9 +24,7 @@ export default function RootLayout({
         <script src="/particles.js"></script>
         <title>Awesome Demostration</title>
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`antialiased`}>
         <StoreProvider>
           <Suspense>{children}</Suspense>
         </StoreProvider>
