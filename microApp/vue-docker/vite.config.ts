9d7303aee4409761d675cad252be3484d5f6ae6d
unstoppable840 <unstoppable840>
2026-02-09 11:32:21 +0800

    modify: 修改vue.config支持环境获取

--- microApp/vue-docker/vite.config.ts
+++ microApp/vue-docker/vite.config.ts
@@ -1,30 +1,33 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import fs from 'fs';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    https: {
-      key: fs.readFileSync('./cert/server.key'),
-      cert: fs.readFileSync('./cert/server.crt'),
-    },
-    proxy: {
-      ['/api']: {
-        target: 'https://localhost:7777',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    server: {
+      https: {
+        key: fs.readFileSync('./cert/server.key'),
+        cert: fs.readFileSync('./cert/server.crt'),
+      },
+      proxy: {
+        ['/api']: {
+          target: `https://localhost:${env.VITE_PUBLIC_NEXT_SERVER_PORT}`,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
